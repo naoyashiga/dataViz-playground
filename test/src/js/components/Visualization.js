cb3cbdc33d0859e6156b3f5978b80c37da4e4d28
naoyashiga <naoyashiga0@gmail.com>
2016-05-22 22:12:48 +0900

    散布図をplot

--- test/src/js/components/Visualization.js
+++ test/src/js/components/Visualization.js
@@ -8,24 +8,83 @@ export default class Visualization {
     this.objects = [];
     this.svg.on('mousedown', function () {
       const mouseCoordinates = d3.mouse(this);
-      _this.addCircle(mouseCoordinates);
+      // _this.addCircle(mouseCoordinates);
     });
 
-    // Add random data
-    this.addRandom();
-    this.addRandom();
-    this.addRandom();
+    this.scatterPlot();
 
     // Callback onLoaded
     cb();
   }
 
-  addRandom() {
-    this.addCircle([Math.random() * 300, Math.random() * 300]);
-  }
+  scatterPlot() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
-  addCircle(mouseCoordinates) {
-    this.objects.push(new Circle(this.svg, mouseCoordinates, config.defaultRadius * Math.random()));
+    var x = d3.scale.linear()
+        .domain([70, 120])
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .domain([70, 120])
+        .range([height, 0]);
+
+    var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    d3.json("datasets/data.json", (error, data) => {
+      console.log(data);
+
+      // data.result.actress.forEach(function(d) {
+      //   d.sepalLength = +d.sepalLength;
+      //   d.sepalWidth = +d.sepalWidth;
+      // });
+      //
+      // x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
+      // y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
+
+
+      this.svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("x", width)
+              .attr("y", -6)
+              .style("text-anchor", "end")
+              .text("Bust (cm)");
+
+      this.svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Hip (cm)")
+
+      this.svg.selectAll(".dot")
+              .data(data.result.actress)
+            .enter().append("circle")
+              .attr("class", "dot")
+              .attr("r", 3.5)
+              .attr("cx", function(d) {
+                console.log(x(d.bust));
+                return x(d.bust); })
+              .attr("cy", function(d) { return y(d.hip); });
+    });
+
+
   }
 
   createViz() {
