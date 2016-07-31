459fa1a3d26593e681420a695cde2cd1188174c0
naoyashiga <naoyashiga0@gmail.com>
2016-07-31 15:41:37 +0900

    add interpolateRgb

--- sketch/tutorial/timeline/test/src/js/components/Circle.js
+++ sketch/tutorial/timeline/test/src/js/components/Circle.js
@@ -1,12 +1,17 @@
 export default class Circle {
   constructor(parentNode, data, xRange) {
+    const colorMap = d3.interpolateRgb(
+      d3.rgb('#d6e685'),
+      d3.rgb('#1e6823')
+    );
+
     this.element = parentNode.selectAll(".node")
     .data(data)
     .enter().append("g")
     .append("circle")
     .attr("cx", function(d) { return xRange(d.time); })
     .attr("cy", "-50")
-    .style("fill", "#000")
+    .style("fill", (d) => colorMap(d.time - 9.5))
     .attr("r", 8);
   }
 }
