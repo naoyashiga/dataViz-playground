d59a116271ba4042c4dad0df16b550e4ae999a48
naoyashiga <naoyashiga0@gmail.com>
2016-08-04 21:49:36 +0900

    y軸ラベルを修正

--- sketch/tutorial/timeline/worldRecord/src/js/components/Screen.js
+++ sketch/tutorial/timeline/worldRecord/src/js/components/Screen.js
@@ -10,16 +10,28 @@ export default class Screen {
     .range([0, width]);
 
     this.yRange = d3.scaleTime()
-    .domain([new Date(2020, 8, 15), new Date(2009, 1, 1), new Date(1968, 1, 1)])
-    .range([0, 120, height - margin.top - margin.bottom]);
+    .domain([
+      new Date(2020, 8, 15),
+      new Date(2009, 1, 1),
+      new Date(1983, 1, 1),
+      new Date(1965, 1, 1)
+    ])
+    .range([
+      0,
+      80,
+      height - margin.top - margin.bottom - 50,
+      height - margin.top - margin.bottom
+    ]);
 
     const xAxis = d3.axisBottom(this.xRange).ticks(20);
 
-    let year = d3.timeYears(new Date(1968, 1, 1), new Date(2009, 1, 1), 1);
+    let year1 = d3.timeYears(new Date(1982, 1, 1), new Date(2009, 1, 1), 1);
 
     let year2 = d3.timeYears(new Date(2015, 1, 1), new Date(2016, 1, 1), 1);
 
-    let myYears = d3.merge([year2, year]);
+    let year3 = d3.timeYears(new Date(1967, 1, 1), new Date(1968, 1, 1), 1);
+
+    let myYears = d3.merge([year1, year2, year3]);
     console.log(myYears);
 
     const yAxis = d3.axisLeft(this.yRange).tickValues(myYears);
